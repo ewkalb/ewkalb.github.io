3666eed787b2c5e7c3ccdbe9656b333e2d83c58e
depthstar <atempovivo@gmail.com>
2024-04-30 23:47:18 -0500

    PRIMARY SKELETON COMPLETE

--- js/script.js
+++ js/script.js
@@ -17,3 +17,43 @@ $(document).ready(function() {
         } 
     });  
   });
+
+$(document).ready(function() {
+    $('#compact').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+});
+
+$(document).ready(function() {
+    $('#full-size').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+});
+
+$(document).ready(function() {
+    $('#rifles').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+});
+
+$(document).ready(function() {
+    $('#shotgun').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+});
